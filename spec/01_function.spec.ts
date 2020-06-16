23adc8a3d73929fa90e71fdc754e5feba1016a92
KYLE X NAYLOR <naylorkx@gmail.com>
2020-06-16 18:03:38 +0000

    array methods

--- spec/01_function.spec.ts
+++ spec/01_function.spec.ts
@@ -48,5 +48,70 @@ describe('functions', () => {
             expect(add(1, 2, 3, 4, 5, 6, 7, 8, 9)).toBe(45);
         });
     });
+});
+
+describe('higher order functions', () => {
+
+    it('basic way to solve the problem', () => {
+        function tagMaker(tag: string, content: string): string {
+            return `<${tag}>${content}</${tag}>`;
+        }
+
+        expect(tagMaker('h1', 'Hello')).toBe('<h1>Hello</h1>');
+        expect(tagMaker('h1', 'Goodbye')).toBe('<h1>Goodbye</h1>');
+        expect(tagMaker('p', 'some content')).toBe('<p>some content</p>');
+    });
+    it('an oop approach', () => {
+        class TagMaker {
+
+            constructor(private tag: string) { }
+
+            make(content: string) {
+                return `<${this.tag}>${content}</${this.tag}>`;
+            }
+        }
+
+        const h1Maker = new TagMaker('h1');
+        const pMaker = new TagMaker('p');
+
+        expect(h1Maker.make('Hello')).toBe('<h1>Hello</h1>');
+        expect(h1Maker.make('Goodbye')).toBe('<h1>Goodbye</h1>');
+        expect(pMaker.make('some content')).toBe('<p>some content</p>');
+    });
+    it('functional style', () => {
+        function tagMaker(tag: string): (content: string) => string {
+            return (content) => `<${tag}>${content}</${tag}>`
+        }
+
+        const h1Maker = tagMaker('h1');
+        const pMaker = tagMaker('p');
+
+        expect(h1Maker('Hello')).toBe('<h1>Hello</h1>');
+        expect(h1Maker('Goodbye')).toBe('<h1>Goodbye</h1>');
+        expect(pMaker('some content')).toBe('<p>some content</p>');
+    });
+});
+
+describe('array methods', () => {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    it('visiting each element in an array', () => {
+        numbers.forEach((e) => console.log(e));
+        numbers.forEach((element, index, collection) => console.log(element, index, collection)) // Eich
+    });
+
+    describe('methods that create new arrays', () => {
+        it('has a filter', () => {
+            // where
+            const evens = numbers.filter(n => n % 2 === 0); // filter returns every element that returns true from the function.
+
+            expect(evens).toEqual([2, 4, 6, 8]);
+            expect(numbers).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+            expect(numbers.filter(() => true)).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+            expect(numbers.filter(() => false)).toEqual([]);
+
+        });
+    });
 
 });
