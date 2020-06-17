4f928aade1d739115315fcce0eea9872b5d763fe
KYLE X NAYLOR <naylorkx@gmail.com>
2020-06-17 16:38:15 +0000

    before lunch

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
-
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
 
+const tipPercentage = .20;
+
+
+
+const btnCalculate = document.getElementById('btnCalculate') as HTMLButtonElement;
+const txtAmount = document.getElementById('txtAmount') as HTMLInputElement;
+const spnBillAmount = document.getElementById('spnBillAmount') as HTMLSpanElement;
+
+console.log({
+    btnCalculate,
+    txtAmount,
+    spnBillAmount
+})
+
+
+btnCalculate.addEventListener('click', handleCalculate);
+
+
+function handleCalculate() {
+    const amount = txtAmount.valueAsNumber;
+    spnBillAmount.innerText = amount.toString();
+    txtAmount.select();
+    txtAmount.focus();
+}
